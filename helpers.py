08bc00a20731360dfcc408832054ca1241bcb5c0
Mojtaba Tabatabaeipour <smt.moji@gmail.com>
2021-09-13 11:27:45 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,1 +1,6 @@
-
+def calculate_non_percentage(df):
+    df_nan_count = df.select(
+    [psf.count(psf.when(psf.col(c).isNull(), True)).alias(c)
+     for c in df.columns])
+    return df_nan_count.toPandas() / df.count()
+ 
